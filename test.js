8183b1b2338ac61a2fcc9e02e5e28489396a9aac
unit-wolf <76440908+unit-wolf@users.noreply.github.com>
2020-12-21 22:14:39 -0500

    Language test

---
+++ test.js
@@ -0,0 +1,68 @@
+
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    return decodeURI(dc.substring(begin + prefix.length, end));
+}
+
+var myCookie = getCookie("language_choice")
+if (myCookie == null) {
+    $('[lang]').hide();
+    $('[lang="en"]').show();
+}
+else
+{
+    if (myCookie == "es") {
+        $('[lang]').hide();
+        $('[lang="es"]').show();
+    }
+    else
+    {
+        $('[lang]').hide();
+        $('[lang="en"]').show();
+    } 
+}
+
+$('#lang-switch').change(function () { // put onchange event when user select option from select
+    var lang = $(this).val(); // decide which language to display using switch case. The rest is obvious (i think)
+    var expiration_date = new Date();
+    var lang_choice = '';
+    expiration_date.setFullYear(expiration_date.getFullYear() + 1);
+    switch (lang) {
+        case 'en':
+            lang_choice = "language_choice=en; expires=" + expiration_date.toUTCString(); + " path=/;"
+            document.cookie = lang_choice;
+            $('[lang]').hide();
+            $('[lang="en"]').show();
+        break;
+        case 'es':
+            lang_choice = "language_choice=es; expires=" + expiration_date.toUTCString(); + " path=/;"
+            document.cookie = lang_choice;
+            $('[lang]').hide();
+            $('[lang="es"]').show();
+        break;
+        default:
+           if (myCookie == 'es') {
+               $('[lang]').hide();
+               $('[lang="es"]').show();
+           }
+           else
+           {
+               $('[lang]').hide();
+               $('[lang="en"]').show();
+           }
+        }
+});
